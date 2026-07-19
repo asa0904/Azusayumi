8848732b8fcc0c5b5271180a3d14c854b058a87b
朝登 釜本 <asa774440@gmail.com>
2026-07-19 10:28:18 +0900

    feat: add color-specific interface and implementations

---
+++ src/Azusayumi.Core/GameLogic/IColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Azusayumi.Core.GameLogic
+{
+    internal interface IColor
+    {
+        internal static abstract bool IsWhite { get; }
+    }
+
+    internal readonly struct White : IColor
+    {
+        public static bool IsWhite
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => true;
+        }
+    }
+
+    internal readonly struct Black : IColor
+    {
+        public static bool IsWhite
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => false;
+        }
+    }
+}
