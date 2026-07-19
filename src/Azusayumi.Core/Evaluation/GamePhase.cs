0ca46ce9ab2a43551a6d563bad189e885a1689cc
朝登 釜本 <asa774440@gmail.com>
2026-07-20 08:51:14 +0900

    feat: add game phase constants

---
+++ src/Azusayumi.Core/Evaluation/GamePhase.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Azusayumi.Core.Evaluation
+{
+    internal static class GamePhase
+    {
+        internal const int Max = 24;
+
+        private static readonly int[] _weights = [0, 1, 1, 2, 4, 0];
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static int GetWeight(int pieceType)
+        {
+            return _weights[pieceType];
+        }
+    }
+}
