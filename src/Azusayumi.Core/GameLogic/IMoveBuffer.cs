8cbc932740d3bb0f7dbba12dd0ebc4f7f544649d
朝登 釜本 <asa774440@gmail.com>
2026-07-19 14:59:10 +0900

    feat: add move buffer interface and implementation

---
+++ src/Azusayumi.Core/GameLogic/IMoveBuffer.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Azusayumi.Core.GameLogic
+{
+    internal interface IMoveBuffer
+    {
+        internal void Add(Move move);
+    }
+
+    internal ref struct MoveBuffer(Span<Move> buffer) : IMoveBuffer
+    {
+        private readonly Span<Move> _buffer = buffer;
+        private int _count;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(Move move)
+        {
+            _buffer[_count++] = move;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal readonly Span<Move> AsSpan()
+        {
+            return _buffer[.._count];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void Clear()
+        {
+            _count = 0;
+        }
+    }
+}
