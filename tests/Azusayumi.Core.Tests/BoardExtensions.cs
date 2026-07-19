e9381e73e95f1d46c10449a63915d9034cbca304
朝登 釜本 <asa774440@gmail.com>
2026-07-19 22:40:41 +0900

    test: add move parsing helper

---
+++ tests/Azusayumi.Core.Tests/BoardExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Azusayumi.Core.GameLogic;
+
+namespace Azusayumi.Core.Tests
+{
+    internal static class BoardExtensions
+    {
+        internal static Move ToMove(this Board board, string stringMove)
+        {
+            MoveBuffer buffer = new(stackalloc Move[256]);
+            MoveGenerator.GenerateLegalMoves(ref buffer, board);
+
+            Span<Move> moves = buffer.AsSpan();
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (moves[i].ToString() == stringMove) { return moves[i]; }
+            }
+
+            throw new ArgumentException($"'{stringMove}' is illegal.");
+        }
+    }
+}
