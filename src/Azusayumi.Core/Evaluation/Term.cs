eec9e24c1b6ef9f33467c0e8b9397136e98cae88
朝登 釜本 <asa774440@gmail.com>
2026-07-21 07:44:42 +0900

    feat: add evaluation term enum

---
+++ src/Azusayumi.Core/Evaluation/Term.cs
@@ -0,0 +1,19 @@
+﻿namespace Azusayumi.Core.Evaluation
+{
+    internal enum Term
+    {
+        Material,
+        PawnPst,
+        KnightPst,
+        BishopPst,
+        RookPst,
+        QueenPst,
+        KingPst,
+        KnightMobility,
+        BishopMobility,
+        RookMobility,
+        QueenMobility,
+
+        Length
+    }
+}
