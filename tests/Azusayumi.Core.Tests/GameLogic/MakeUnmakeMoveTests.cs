b3d42f3266f37a8d579fd48ab7e1cd6e12364435
朝登 釜本 <asa774440@gmail.com>
2026-07-20 20:35:18 +0900

    test: add PST score update tests

--- tests/Azusayumi.Core.Tests/GameLogic/MakeUnmakeMoveTests.cs
+++ tests/Azusayumi.Core.Tests/GameLogic/MakeUnmakeMoveTests.cs
@@ -54,6 +54,26 @@ namespace Azusayumi.Core.Tests.GameLogic
         [InlineData("Castling move",  "r1bqkb1r/1ppp1ppp/p1n2n2/4p3/B3P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 1", "e1h1")]
         [InlineData("EnPassant move", "4k3/8/8/3pP3/8/8/8/4K3 w - d6 0 1",                                   "e5d6")]
         [InlineData("Promotion move", "5b2/3KPk2/8/8/8/8/8/8 w - - 0 1",                                     "e7e8q")]
+        public void MakeMove_SingleMove_MatchesRecalculatedPstScore(string scenario, string fen, string uciMove)
+        {
+            Board board = new(fen, historyCapacity: 2);
+            Move  move  = board.ToMove(uciMove);
+
+            board.MakeMove<White>(move);
+
+            Score expectedScore = CalculatePstScore(board);
+            Score actualScore   = board.Score;
+
+            Assert.True(expectedScore.Mid == actualScore.Mid
+                     && expectedScore.End == actualScore.End, $"Failed test: {scenario}");
+        }
+
+        [Theory]
+        [InlineData("Quiet move",     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",            "d2d4")]
+        [InlineData("Capture move",   "rnbqk2r/ppp2ppp/4pn2/3p4/2PP4/2b1PN2/PP3PPP/R1BQKB1R w KQkq - 0 1",   "b2c3")]
+        [InlineData("Castling move",  "r1bqkb1r/1ppp1ppp/p1n2n2/4p3/B3P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 1", "e1h1")]
+        [InlineData("EnPassant move", "4k3/8/8/3pP3/8/8/8/4K3 w - d6 0 1",                                   "e5d6")]
+        [InlineData("Promotion move", "5b2/3KPk2/8/8/8/8/8/8 w - - 0 1",                                     "e7e8q")]
         public void UnmakeMove_SingleMove_RestoresOriginalBoard(string scenario, string fen, string stringMove)
         {
             Board board = new(fen, historyCapacity: 2);
@@ -110,6 +130,29 @@ namespace Azusayumi.Core.Tests.GameLogic
             }
 
             return phase;
+        }
+
+        private static Score CalculatePstScore(Board board)
+        {
+            Score score = Score.Zero;
+
+            ulong pieces = board.GetFriends<White>();
+            while (pieces != 0)
+            {
+                int squareIndex = Bitboard.PopLsb(ref pieces);
+                int pieceType   = board.GetPieceType(squareIndex);
+                score += PieceSquareTables.GetScore<White>(pieceType, squareIndex);
+            }
+
+            pieces = board.GetFriends<Black>();
+            while (pieces != 0)
+            {
+                int squareIndex = Bitboard.PopLsb(ref pieces);
+                int pieceType   = board.GetPieceType(squareIndex);
+                score += PieceSquareTables.GetScore<Black>(pieceType, squareIndex);
+            }
+
+            return score;
         }
     }
 }
