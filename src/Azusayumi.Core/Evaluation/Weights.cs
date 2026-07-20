0d243d364a7c1b1473b69a68b4444c9eaf24b819
朝登 釜本 <asa774440@gmail.com>
2026-07-21 07:48:50 +0900

    feat: add evaluation weights

---
+++ src/Azusayumi.Core/Evaluation/Weights.cs
@@ -0,0 +1,18 @@
+﻿namespace Azusayumi.Core.Evaluation
+{
+    internal class Weights
+    {
+        internal static readonly Score[] Material  = new Score[5];
+        internal static readonly Score[] PawnPst   = new Score[64];
+        internal static readonly Score[] KnightPst = new Score[64];
+        internal static readonly Score[] BishopPst = new Score[64];
+        internal static readonly Score[] RookPst   = new Score[64];
+        internal static readonly Score[] QueenPst  = new Score[64];
+        internal static readonly Score[] KingPst   = new Score[64];
+
+        internal static readonly Score[] KnightMobility = new Score[9];
+        internal static readonly Score[] BishopMobility = new Score[14];
+        internal static readonly Score[] RookMobility   = new Score[15];
+        internal static readonly Score[] QueenMobility  = new Score[28];
+    }
+}
