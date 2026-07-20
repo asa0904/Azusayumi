e909e09d7f188249e259f77ea297f1465e5f42b3
朝登 釜本 <asa774440@gmail.com>
2026-07-20 20:22:33 +0900

    test: add game phase update tests

--- tests/Azusayumi.Core.Tests/GameLogic/MakeUnmakeMoveTests.cs
+++ tests/Azusayumi.Core.Tests/GameLogic/MakeUnmakeMoveTests.cs
@@ -1,4 +1,5 @@
-﻿using Azusayumi.Core.GameLogic;
+﻿using Azusayumi.Core.Evaluation;
+using Azusayumi.Core.GameLogic;
 
 namespace Azusayumi.Core.Tests.GameLogic
 {
@@ -21,6 +22,30 @@ namespace Azusayumi.Core.Tests.GameLogic
             ulong actual   = board.Key;
 
             Assert.True(expected == actual, $"Failed test: {scenario}");
+        }
+
+        [Theory]
+        [InlineData("Quiet move",     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",            "d2d4")]
+        [InlineData("Capture move",   "rnbqk2r/ppp2ppp/4pn2/3p4/2PP4/2b1PN2/PP3PPP/R1BQKB1R w KQkq - 0 1",   "b2c3")]
+        [InlineData("Castling move",  "r1bqkb1r/1ppp1ppp/p1n2n2/4p3/B3P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 1", "e1h1")]
+        [InlineData("EnPassant move", "4k3/8/8/3pP3/8/8/8/4K3 w - d6 0 1",                                   "e5d6")]
+        [InlineData("Promotion move", "5b2/3KPk2/8/8/8/8/8/8 w - - 0 1",                                     "e7e8q")]
+        public void MakeMove_SingleMove_MatchesRecalculatedGamePhase(string scenario, string fen, string stringMove)
+        {
+            Board board = new(fen, historyCapacity: 2);
+            Move  move  = board.ToMove(stringMove);
+
+            board.MakeMove<White>(move);
+
+            int expectedWhitePhase = board.GetPhase<White>();
+            int actualWhitePhase   = CalculatePhase(isWhite: true, board);
+            
+            Assert.True(expectedWhitePhase == actualWhitePhase, $"Failed test: {scenario}");
+
+            int expectedBlackPhase = board.GetPhase<Black>();
+            int actualBlackPhase   = CalculatePhase(isWhite: false, board);
+            
+            Assert.True(expectedBlackPhase == actualBlackPhase, $"Failed test: {scenario}");
         }
 
         [Theory]
@@ -73,6 +98,18 @@ namespace Azusayumi.Core.Tests.GameLogic
             }
 
             return key;
+        }
+
+        private static int CalculatePhase(bool isWhite, Board board)
+        {
+            int phase = 0;
+            for (int pieceType = PieceType.Pawn; pieceType < PieceType.Length; pieceType++)
+            {
+                ulong pieces = isWhite ? board.GetFriends<White>(pieceType) : board.GetFriends<Black>(pieceType);
+                phase += Bitboard.PopCount(pieces) * GamePhase.GetWeight(pieceType);
+            }
+
+            return phase;
         }
     }
 }
