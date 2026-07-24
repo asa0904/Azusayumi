9ae03e127d753f3634063f86432e4013c5077446
朝登 釜本 <asa774440@gmail.com>
2026-07-24 18:36:05 +0900

    feat: implement evaluator

---
+++ src/Azusayumi.Core/Evaluation/Evaluator.cs
@@ -0,0 +1,48 @@
+﻿using Azusayumi.Core.GameLogic;
+using System.Runtime.CompilerServices;
+
+namespace Azusayumi.Core.Evaluation
+{
+    internal static class Evaluator
+    {
+        private struct SearchContext : IEvaluationContext
+        {
+            private Score _score;
+
+            internal readonly Score Score
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => _score;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Add<TColor>(Term term, int index = 0, int count = 1) where TColor : struct, IColor
+            {
+                if (TColor.IsWhite) { _score += GetWeight(term, index, count); }
+                else                { _score -= GetWeight(term, index, count); }
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private static Score GetWeight(Term term, int index, int count)
+            {
+                return term switch
+                {
+                    Term.KnightMobility => Weights.KnightMobility[index],
+                    Term.BishopMobility => Weights.BishopMobility[index],
+                    Term.RookMobility   => Weights.RookMobility[index],
+                    Term.QueenMobility  => Weights.QueenMobility[index],
+                    _ => Score.Zero
+                };
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static int Evaluate(Board board)
+        {
+            SearchContext context = Evaluator<SearchContext>.Evaluate(board, default);
+            Score score = board.Score + context.Score;
+
+            return score.Interpolate(board.GetPhase());
+        }
+    }
+}
