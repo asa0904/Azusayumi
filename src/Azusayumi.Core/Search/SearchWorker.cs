bf3ad3ab270e10cbc860c92dd05e25ca64802b1d
朝登 釜本 <asa774440@gmail.com>
2026-08-07 17:00:58 +0900

    feat: add SearchWorker class

---
+++ src/Azusayumi.Core/Search/SearchWorker.cs
@@ -0,0 +1,15 @@
+﻿using Azusayumi.Core.GameLogic;
+
+namespace Azusayumi.Core.Search
+{
+    internal partial class SearchWorker
+    {
+        internal const int MaxPly = 64;
+
+        private const int Infinity  = short.MaxValue;
+        private const int MateValue = 10000;
+        private const int DrawValue = 0;
+
+        private readonly Board _board = new();
+    }
+}
