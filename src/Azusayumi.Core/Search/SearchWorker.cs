c97df75f178fc9839b298225483c870b5a7cac67
朝登 釜本 <asa774440@gmail.com>
2026-08-07 17:08:12 +0900

    feat: add IMoveBuffer implementation for search

--- src/Azusayumi.Core/Search/SearchWorker.cs
+++ src/Azusayumi.Core/Search/SearchWorker.cs
@@ -1,4 +1,5 @@
 ﻿using Azusayumi.Core.GameLogic;
+using System.Runtime.CompilerServices;
 
 namespace Azusayumi.Core.Search
 {
@@ -11,5 +12,35 @@ namespace Azusayumi.Core.Search
         private const int DrawValue = 0;
 
         private readonly Board _board = new();
+
+        private ref struct MoveBuffer : IMoveBuffer
+        {
+            private readonly Span<ScoredMove> _buffer;
+            private int _count;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal MoveBuffer(Span<ScoredMove> buffer)
+            {
+                _buffer = buffer;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Add(Move move)
+            {
+                _buffer[_count++].Move = move;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal readonly Span<ScoredMove> AsSpan()
+            {
+                return _buffer[.._count];
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal void Clear()
+            {
+                _count = 0;
+            }
+        }
     }
 }
