25eedd48b2b0b3f2e461bfacfc397c73e000b259
朝登 釜本 <asa774440@gmail.com>
2026-08-07 17:01:16 +0900

    feat: implement PV table

---
+++ src/Azusayumi.Core/Search/PVTable.cs
@@ -0,0 +1,49 @@
+﻿using Azusayumi.Core.GameLogic;
+using System.Runtime.CompilerServices;
+
+namespace Azusayumi.Core.Search
+{
+    internal class PVTable
+    {
+        private readonly int[]  _lengths = new int[SearchWorker.MaxPly + 1];
+        private readonly Move[] _moves   = new Move[(SearchWorker.MaxPly * SearchWorker.MaxPly) + 1];
+
+        internal Move BestMove
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _moves[0];
+        }
+
+        internal Move PonderMove
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _moves[1];
+        }
+
+        internal ReadOnlySpan<Move> PV
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _moves.AsSpan()[.._lengths[0]];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void Write(int ply, Move move)
+        {
+            int length = _lengths[ply + 1] + 1;
+            _lengths[ply] = length;
+
+            int targetIndex = ply * SearchWorker.MaxPly;
+            int sourceIndex = targetIndex + SearchWorker.MaxPly;
+            _moves[targetIndex++] = move;
+            Span<Move>         target = _moves.AsSpan().Slice(targetIndex, length);
+            ReadOnlySpan<Move> source = _moves.AsSpan().Slice(sourceIndex, length);
+            source.CopyTo(target);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void Clear(int ply)
+        {
+            _lengths[ply] = 0;
+        }
+    }
+}
