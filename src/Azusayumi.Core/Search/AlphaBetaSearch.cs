d8e770ec7061cf10e9ec535bfb6fea4b8f302df8
朝登 釜本 <asa774440@gmail.com>
2026-08-07 17:19:54 +0900

    feat: implement alpha-beta search

---
+++ src/Azusayumi.Core/Search/AlphaBetaSearch.cs
@@ -0,0 +1,54 @@
+﻿using Azusayumi.Core.GameLogic;
+
+namespace Azusayumi.Core.Search
+{
+    internal partial class SearchWorker
+    {
+        private int AlphaBetaSearch<TColor>(int depth, int ply, int alpha, int beta) where TColor : struct, IColor
+        {
+            if (depth == 0) { return Quiesce<TColor>(ply, alpha, beta); }
+
+            if (_board.IsDraw() || ply >= MaxPly) { return DrawValue; }
+
+            int  bestValue = -Infinity;
+            bool isInCheck = _board.IsInCheck<TColor>();
+
+            MoveBuffer buffer = new(_moveArrayPool.GetSpan(ply));
+            MoveGenerator<TColor>.GenerateLegalMoves(ref buffer, _board, isInCheck);
+            Span<ScoredMove> scoredMoves = buffer.AsSpan();
+            MoveOrdering.ScoreCaptures(scoredMoves, _board);
+
+            for (int i = 0; i < scoredMoves.Length; i++)
+            {
+                Move move = MoveOrdering.Select(i, scoredMoves);
+
+                _board.MakeMove<TColor>(move);
+                int value = -OppositeAlphaBetaSearch<TColor>(depth - 1, ply + 1, -beta, -alpha);
+                _board.UnmakeMove<TColor>(move);
+
+                if (value > bestValue)
+                {
+                    bestValue = value;
+
+                    if (value > alpha)
+                    {
+                        if (value >= beta) { break; }
+
+                        alpha = value;
+                    }
+                }
+            }
+
+            if (scoredMoves.Length == 0) { return _board.IsInCheck<TColor>() ? -MateValue + ply : DrawValue; }
+
+            return bestValue;
+        }
+
+        private int OppositeAlphaBetaSearch<TColor>(int depth, int ply, int alpha, int beta)
+            where TColor : struct, IColor
+        {
+            return TColor.IsWhite ? AlphaBetaSearch<Black>(depth, ply, alpha, beta)
+                                  : AlphaBetaSearch<White>(depth, ply, alpha, beta);
+        }
+    }
+}
