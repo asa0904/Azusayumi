8a56f4c501be4d619d07c8768d51ef3a62298eed
朝登 釜本 <asa774440@gmail.com>
2026-08-27 11:58:23 +0900

    feat: add SearchInfo struct

---
+++ src/Azusayumi.Core/Search/SearchInfo.cs
@@ -0,0 +1,13 @@
+﻿using Azusayumi.Core.GameLogic;
+
+namespace Azusayumi.Core.Search
+{
+    internal record struct SearchInfo(
+        int  Depth,
+        int  HighestDepth,
+        int  Score,
+        long Nodes,
+        long Time,
+        Move BestMove,
+        Move PonderMove);
+}
