c3837505f6bcacbcf5df4b3dfe1a825f70c99797
朝登 釜本 <asa774440@gmail.com>
2026-08-27 12:00:42 +0900

    feat: add SearchConditions struct

---
+++ src/Azusayumi.Core/Search/SearchConditions.cs
@@ -0,0 +1,9 @@
+﻿namespace Azusayumi.Core.Search
+{
+    internal readonly record struct SearchConditions(
+        int  Depth,
+        int  Time,
+        int  Inc,
+        int  MoveTime,
+        long Nodes);
+}
