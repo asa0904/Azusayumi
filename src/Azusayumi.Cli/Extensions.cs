4798909afe9b8dfbb20f5ecaca2ac95eec7ee0bb
朝登 釜本 <asa774440@gmail.com>
2026-08-31 16:25:19 +0900

    feat: add ConsumeTo extension method for ReadOnlySpan<char>

---
+++ src/Azusayumi.Cli/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Azusayumi.Cli
+{
+    internal static class ReadOnlySpanExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static ReadOnlySpan<char> ConsumeTo(this ReadOnlySpan<char> source, char separator, out ReadOnlySpan<char> token)
+        {
+            int index = source.IndexOf(separator);
+            if (index == -1)
+            {
+                token = source;
+                return default;
+            }
+            else
+            {
+                token = source[..index];
+                return source[(index + 1)..];
+            }
+        }
+    }
+}
