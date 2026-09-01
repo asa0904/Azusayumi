998d79a8461008876fd1842dd54d251c7f251b88
朝登 釜本 <asa774440@gmail.com>
2026-09-01 10:42:50 +0900

    feat: implement UCI engine

---
+++ src/Azusayumi.Cli/UciEngine.cs
@@ -0,0 +1,145 @@
+﻿using Azusayumi.Core.GameLogic;
+using Azusayumi.Core.Search;
+using System.Reflection;
+
+namespace Azusayumi.Cli
+{
+    internal class UciEngine
+    {
+        private bool _isSearching;
+        private bool _exitEngine;
+        private SearchConditions _conditions;
+        private readonly Board _board = new();
+        private readonly SearchManager _searchManager = new();
+        private readonly ManualResetEventSlim _searchStartEvent  = new(initialState: false);
+        private readonly ManualResetEventSlim _searchFinishEvent = new(initialState: true);
+
+        private record struct GoCommand(int Depth, int MoveTime, int Nodes, int TotalTime, int Inc);
+
+        internal UciEngine()
+        {
+            Thread searchThread = new(SearchLoop)
+            {
+                IsBackground = true,
+                Name = "SearchThread",
+                Priority = ThreadPriority.Normal,
+            };
+            searchThread.Start();
+        }
+
+        internal static void PrintUciInfo()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string? version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+            Console.WriteLine($"id name Azusayumi {version}");
+            Console.WriteLine("id author Asato Kamamoto");
+        }
+
+        internal void Clear()
+        {
+            Stop();
+            _board.Set("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+            _searchManager.CopyPosition(_board);
+        }
+
+        internal void SetPosition(ReadOnlySpan<char> fen, ReadOnlySpan<char> uciMoves)
+        {
+            _board.Set(fen);
+
+            MoveBuffer buffer = new(stackalloc Move[256]);
+            while (!uciMoves.IsEmpty)
+            {
+                Move move = Move.Null;
+                uciMoves = uciMoves.ConsumeTo(' ', out ReadOnlySpan<char> uciMove);
+
+                buffer.Clear();
+                MoveGenerator.GenerateLegalMoves(ref buffer, _board);
+                Span<Move> legalMoves = buffer.AsSpan();
+
+                for (int i = 0; i < legalMoves.Length; i++)
+                {
+                    if (legalMoves[i].Equals(uciMove)) { move = legalMoves[i]; break; }
+                }
+
+                if (move != Move.Null)
+                {
+                    _board.MakeMove(move);
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid move sequence: '{uciMove}' is illegal. Ignoring subsequent moves.");
+                    break;
+                }
+            }
+
+            _searchManager.CopyPosition(_board);
+        }
+
+        internal void Search(UciGoOptions options)
+        {
+            _conditions = new SearchConditions()
+            {
+                Depth    = options.Depth,
+                Time     = _board.IsWhiteToMove ? options.Wtime : options.Btime,
+                Inc      = _board.IsWhiteToMove ? options.Winc  : options.Binc,
+                MoveTime = options.MoveTime,
+                Nodes    = options.Nodes,
+            };
+
+            _isSearching = true;
+            _searchStartEvent.Set();
+            _searchFinishEvent.Reset();
+        }
+
+        internal void Stop()
+        {
+            _isSearching = false;
+            _searchManager.Stop();
+            _searchFinishEvent.Wait();
+            _searchStartEvent.Reset();
+        }
+
+        internal void Quit()
+        {
+            _exitEngine = true;
+            _isSearching = false;
+            _searchManager.Stop();
+            _searchFinishEvent.Wait();
+            _searchStartEvent.Set();
+        }
+
+        internal void PrintPosition()
+        {
+            _board.Print();
+        }
+
+        private void SearchLoop()
+        {
+            Span<char> answer = new char[32];
+            "bestmove ".CopyTo(answer);
+
+            while (!_exitEngine)
+            {
+                _searchStartEvent.Wait();
+
+                if (_exitEngine) { return; }
+                if (!_isSearching) { continue; }
+
+                SearchInfo result = _searchManager.Search<UciLogger>(_conditions);
+
+                int offset = "bestmove ".Length;
+                result.BestMove.Format(answer[offset..], out int written);
+                offset += written;
+                " ponder ".CopyTo(answer[offset..]);
+                offset += " ponder ".Length;
+                result.PonderMove.Format(answer[offset..], out _);
+
+                Console.WriteLine(answer);
+
+                _searchStartEvent.Reset();
+                _searchFinishEvent.Set();
+            }
+        }
+    }
+}
