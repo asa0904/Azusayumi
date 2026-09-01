798e2ad5b5613b385704693fc179d9fe0895ce09
朝登 釜本 <asa774440@gmail.com>
2026-09-01 13:51:51 +0900

    test: add positions for tuning tests

---
+++ tests/Azusayumi.Tuning.GD.Tests/Fens.cs
@@ -0,0 +1,41 @@
+﻿namespace Azusayumi.Tuning.GD.Tests
+{
+    public static class Fens
+    {
+        public static TheoryData<string> GetTestPositions()
+        {
+            return new([
+                "8/1n6/1p6/p2k4/P6B/K7/8/8 w - - 0 1",
+                "4r2k/7p/2b2q2/3p1P2/p2P1Q2/PpP1P2P/5RB1/6K1 b - - 0 1",
+                "1r6/4p1k1/4p3/2p1p3/2P3Pp/1r2P2P/8/4RRK1 w - - 0 1",
+                "1r5k/6pb/3q1p2/p1p1p1pP/1pQnP1P1/1P2NP2/PKP3B1/3R4 w - - 0 1",
+                "8/4k3/8/1K1B4/4N1b1/8/8/4b3 w - - 0 1",
+                "8/8/8/6R1/4q3/4k2K/8/8 w - - 0 1",
+                "8/8/8/1P6/3NK3/p1k5/8/8 w - - 0 1",
+                "8/6p1/5p2/5P1k/2R5/8/1r4r1/4R1K1 w - - 0 1",
+                "8/1p6/1Pp5/2Pp1k1K/r2P3p/5P1P/p7/R7 w - - 0 1",
+                "8/2P5/8/2KBkb2/8/8/8/8 w - - 0 1",
+                "4R3/8/6P1/5P2/4n3/3kN1K1/r7/8 w - - 0 1",
+                "8/5pk1/1p5r/p1pR2p1/P1P1Pb2/1P3P1P/5K2/5B2 b - - 0 1",
+                "2b2rk1/1rq1p2p/6pb/p2pPp2/3B1n2/P6P/1PPN1QP1/2KR1B1R b - - 0 1",
+                "5k2/N4ppp/1B1p4/3Pp3/Pb6/3b3P/B6K/8 w - - 0 1",
+                "8/8/3K1pk1/8/8/8/6Q1/4r3 b - - 0 1",
+                "2r1r1k1/5p2/p2q2pp/1p1nb3/4p3/1PB1P1P1/P1R1QPBP/2R3K1 w - - 0 1",
+                "r1bq1rk1/pp1nbppp/2p2n2/4p3/8/2NP1NP1/PP1BPPBP/2RQ1RK1 w - - 0 1",
+                "8/5p1k/8/3q3P/pR6/P7/KP6/8 w - - 0 1",
+                "2r2qk1/1p2b1p1/p2pb2p/8/2PQP3/N6P/PP2B1P1/R5K1 b - - 0 1",
+                "8/8/4k3/p4p1p/5K1P/PP6/8/8 b - - 0 1",
+                "8/p7/1b1R1k2/5p2/2P3p1/6PP/r7/5B1K b - - 0 1",
+                "R7/8/8/3K1p1k/P7/1P6/2r5/8 b - - 0 1",
+                "r4rk1/1p3pp1/1bp2q1p/3b4/BP1p4/3Q3P/2PB1PP1/R3R1K1 w - - 0 1",
+                "8/2Q5/P7/8/3P2q1/3K3k/P2B3p/8 w - - 0 1",
+                "6k1/2p2pp1/1p3q1p/1P1Bpn2/2P5/P6P/5QP1/4R2K w - - 0 1",
+                "r2qr1k1/pppnbpp1/3p1n1p/5b2/2P2P2/1PN3P1/P2QPNBP/R1B2RK1 b - - 0 1",
+                "8/5k2/P6R/6p1/6P1/r7/5K2/8 b - - 0 1",
+                "r2q1rk1/1p2ppbp/p1p3p1/3nN3/N2Pb3/1P4P1/P2QPP1P/R1B2RK1 w - - 0 1",
+                "r1b1k2r/1p2qppp/p2bp3/2p5/1n6/1PN2NP1/P2PPPBP/RQR3K1 b kq - 0 1",
+                "2r1k2r/pp1n1p2/q4npp/8/3ppP1Q/PP1P2PP/1KP1N1B1/R2R4 b k - 0 1",
+            ]);
+        }
+    }
+}
