d424e895184bd1b0a6ea534af45c03d279f4caf0
朝登 釜本 <asa774440@gmail.com>
2026-09-01 10:41:22 +0900

    feat: add UCI move notation utilities

--- src/Azusayumi.Cli/Extensions.cs
+++ src/Azusayumi.Cli/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Azusayumi.Core.GameLogic;
+using System.Runtime.CompilerServices;
 
 namespace Azusayumi.Cli
 {
@@ -17,6 +18,69 @@ namespace Azusayumi.Cli
             {
                 token = source[..index];
                 return source[(index + 1)..];
+            }
+        }
+    }
+
+    internal static class MoveExtensions
+    {
+        internal static bool Equals(this Move move, ReadOnlySpan<char> chars)
+        {
+            if (move.Type == MoveType.Promotion)
+            {
+                if (chars.Length < 5) { return false; }
+
+                char promotionType = "pnbrq"[move.PromotionType];
+                if (chars[4] != promotionType) { return false; }
+            }
+            else
+            {
+                if (chars.Length != 4) { return false; }
+            }
+
+            int originIndex = move.OriginIndex;
+            int targetIndex = move.TargetIndex;
+
+            if (move.Type == MoveType.Castling)
+            {
+                targetIndex = originIndex < targetIndex ? originIndex + 2 : originIndex - 2;
+            }
+
+            if (chars[3] != (char)('1' + Square.GetRank(targetIndex))) { return false; }
+            if (chars[2] != (char)('a' + Square.GetFile(targetIndex))) { return false; }
+            if (chars[1] != (char)('1' + Square.GetRank(originIndex))) { return false; }
+            if (chars[0] != (char)('a' + Square.GetFile(originIndex))) { return false; }
+
+            return true;
+        }
+
+        internal static void Format(this Move move, Span<char> destination, out int written)
+        {
+            if (move == Move.Null)
+            {
+                "0000".CopyTo(destination);
+                written = 4;
+                return;
+            }
+
+            int originIndex = move.OriginIndex;
+            int targetIndex = move.TargetIndex;
+
+            if (move.Type == MoveType.Castling)
+            {
+                targetIndex = originIndex < targetIndex ? originIndex + 2 : originIndex - 2;
+            }
+
+            destination[3] = (char)('1' + Square.GetRank(targetIndex));
+            destination[2] = (char)('a' + Square.GetFile(targetIndex));
+            destination[1] = (char)('1' + Square.GetRank(originIndex));
+            destination[0] = (char)('a' + Square.GetFile(originIndex));
+            written = 4;
+
+            if (move.Type == MoveType.Promotion)
+            {
+                destination[4] = "pnbrq"[move.PromotionType];
+                written = 5;
             }
         }
     }
