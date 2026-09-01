1851446b78952ca89922f58a753dcfab4c3c4135
朝登 釜本 <asa774440@gmail.com>
2026-09-01 12:41:27 +0900

    feat: add floating-point evaluator

---
+++ tuning/Azusayumi.Tuning.GD/Evaluator.cs
@@ -0,0 +1,20 @@
+﻿using Azusayumi.Core.Evaluation;
+using Azusayumi.Core.GameLogic;
+
+namespace Azusayumi.Tuning.GD
+{
+    internal static class Evaluator
+    {
+        internal static double Evaluate(Board board, TuningContext context)
+        {
+            context = Evaluator<TuningContext>.EvaluatePst(board, context);
+            context = Evaluator<TuningContext>.Evaluate(board, context);
+
+            Score  score  = context.Score;
+            double phase  = (double)board.GetPhase() / GamePhase.Max;
+            double result = (score.Mid * phase) + (score.End * (1.0 - phase));
+
+            return result;
+        }
+    }
+}
