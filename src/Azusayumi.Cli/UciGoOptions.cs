012dcb10ebb67d7d16d0ec30fa7e5ab848ce1fdd
朝登 釜本 <asa774440@gmail.com>
2026-09-01 10:42:36 +0900

    feat: add UciGoOptions struct

---
+++ src/Azusayumi.Cli/UciGoOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Azusayumi.Cli
+{
+    internal record struct UciGoOptions(
+        int Depth,
+        int MoveTime,
+        int Nodes,
+        int Wtime,
+        int Btime,
+        int Winc,
+        int Binc);
+}
