f6cf6f5da6c6ded6b9d4178f66d954e93091ab34
朝登 釜本 <asa774440@gmail.com>
2026-09-01 10:43:12 +0900

    feat: implement UCI responses

--- src/Azusayumi.Cli/Program.cs
+++ src/Azusayumi.Cli/Program.cs
@@ -2,9 +2,144 @@
 {
     internal class Program
     {
+        private static bool _isRunning = true;
+        private static readonly UciEngine _engine = new();
+
         internal static void Main()
         {
+            Console.InputEncoding  = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            _engine.Clear();
+
+            while (_isRunning)
+            {
+                string? input = Console.ReadLine();
+                if (input == null) { continue; }
+
+                try
+                {
+                    ProcessCommand(input);
+                }
+                catch (Exception ex)
+                {
+                    _engine.Clear();
+                    Console.WriteLine($"An error occurred while processing the UCI command: {ex.Message}");
+                }
+            }
+        }
+
+        private static void ProcessCommand(string command)
+        {
+            ReadOnlySpan<char> tokens = command.AsSpan().Trim().ConsumeTo(' ', out ReadOnlySpan<char> token);
+
+            if (token.SequenceEqual("position"))
+            {
+                SetPosition(tokens);
+            }
+            else if (token.SequenceEqual("go"))
+            {
+                StartSearching(tokens);
+            }
+            else if (token.SequenceEqual("stop"))
+            {
+                _engine.Stop();
+            }
+            else if (token.SequenceEqual("uci"))
+            {
+                UciEngine.PrintUciInfo();
+                Console.WriteLine("uciok");
+            }
+            else if (token.SequenceEqual("isready"))
+            {
+                Console.WriteLine("readyok");
+            }
+            else if (token.SequenceEqual("ucinewgame"))
+            {
+                _engine.Clear();
+            }
+            else if (token.SequenceEqual("quit"))
+            {
+                _engine.Quit();
+                _isRunning = false;
+            }
+            else if (token.SequenceEqual("draw"))
+            {
+                _engine.PrintPosition();
+            }
+            else
+            {
+                Console.WriteLine("Unknown command.");
+            }
+        }
+
+        private static void SetPosition(ReadOnlySpan<char> tokens)
+        {
+            tokens = tokens.ConsumeTo(' ', out ReadOnlySpan<char> token);
+            int moveIndex = tokens.IndexOf("moves", StringComparison.OrdinalIgnoreCase);
+
+            ReadOnlySpan<char> fen;
+            if (token.SequenceEqual("startpos"))
+            {
+                fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".AsSpan();
+            }
+            else if (token.SequenceEqual("fen"))
+            {
+                fen = moveIndex == -1 ? tokens : tokens[..moveIndex].TrimEnd();
+            }
+            else { return; }
+
+            ReadOnlySpan<char> moves = moveIndex == -1 ? default : tokens[(moveIndex + "moves".Length + 1)..].TrimStart();
+
+            _engine.SetPosition(fen, moves);
+        }
+
+        private static void StartSearching(ReadOnlySpan<char> tokens)
+        {
+            UciGoOptions options = new();
+
+            while (!tokens.IsEmpty)
+            {
+                tokens = tokens.ConsumeTo(' ', out ReadOnlySpan<char> token);
+
+                if (token.SequenceEqual("depth"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int depth)) { options.Depth = depth; }
+                }
+                else if (token.SequenceEqual("movetime"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int movetime)) { options.MoveTime = movetime; }
+                }
+                else if (token.SequenceEqual("nodes"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int nodes)) { options.Nodes = nodes; }
+                }
+                else if (token.SequenceEqual("wtime"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int wtime)) { options.Wtime = wtime; }
+                }
+                else if (token.SequenceEqual("btime"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int btime)) { options.Btime = btime; }
+                }
+                else if (token.SequenceEqual("winc"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int winc)) { options.Winc = winc; }
+                }
+                else if (token.SequenceEqual("binc"))
+                {
+                    tokens = tokens.ConsumeTo(' ', out token);
+                    if (int.TryParse(token, out int binc)) { options.Binc = binc; }
+                }
+            }
+
+            _engine.Search(options);
         }
     }
 }
