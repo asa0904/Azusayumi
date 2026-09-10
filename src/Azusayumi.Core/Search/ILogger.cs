0e52ab0019ef0625348fccfc9ceb92caa2574ade
朝登 釜本 <asa774440@gmail.com>
2026-09-10 13:36:02 +0900

    feat: add NullLogger struct

--- src/Azusayumi.Core/Search/ILogger.cs
+++ src/Azusayumi.Core/Search/ILogger.cs
@@ -1,4 +1,5 @@
 ﻿using Azusayumi.Core.GameLogic;
+using System.Runtime.CompilerServices;
 
 namespace Azusayumi.Core.Search
 {
@@ -11,5 +12,32 @@ namespace Azusayumi.Core.Search
         static abstract void LogCurrentMove(int depth, Move move, int moveCount);
 
         static abstract void LogException(Exception ex);
+    }
+
+    internal readonly struct NullLogger : ILogger
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogFullInfo(SearchInfo info, ReadOnlySpan<Move> pv)
+        {
+            return;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogBestMove(SearchResult result)
+        {
+            return;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogCurrentMove(int depth, Move move, int moveCount)
+        {
+            return;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogException(Exception ex)
+        {
+            return;
+        }
     }
 }
