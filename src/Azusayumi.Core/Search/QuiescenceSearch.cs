10b7ef05451decd90e506606af37c2a5e3834699
朝登 釜本 <asa774440@gmail.com>
2026-09-11 23:47:23 +0900

    feat: implement null-window quiescence search

--- src/Azusayumi.Core/Search/QuiescenceSearch.cs
+++ src/Azusayumi.Core/Search/QuiescenceSearch.cs
@@ -65,5 +65,57 @@ namespace Azusayumi.Core.Search
 
             return bestValue;
         }
+
+        private int QuiesceNonPV<TColor>(int ply, int beta) where TColor : struct, IColor
+        {
+            _nodes++;
+
+#if COLLECT_STATS
+            _statistics.QuiescentNodes++;
+#endif
+
+            if (ply > _highestDepth) { _highestDepth = ply; }
+
+            if (_board.IsDraw() || ply >= MaxPly) { return DrawValue; }
+
+            bool isInCheck = _board.IsInCheck<TColor>();
+            int  standPat  = isInCheck ? -Infinity : Evaluator.Evaluate<TColor>(_board);
+            int  bestValue = standPat;
+
+            if (standPat >= beta) { return standPat; }
+
+            MoveBuffer buffer = new(_moveArrayPool.GetSpan(ply));
+            if (isInCheck) { MoveGenerator<TColor>.GenerateEvasionMoves(ref buffer, _board); }
+            else           { MoveGenerator<TColor>.GenerateTacticalMoves(ref buffer, _board); }
+            Span<ScoredMove> scoredMoves = buffer.AsSpan();
+            MoveOrdering.ScoreCaptures(scoredMoves, _board);
+
+            for (int i = 0; i < scoredMoves.Length; i++)
+            {
+                Move move = MoveOrdering.Select(i, scoredMoves);
+
+                _board.MakeMove<TColor>(move);
+                int value = TColor.IsWhite ? -QuiesceNonPV<Black>(ply + 1, -(beta - 1)) : -QuiesceNonPV<White>(ply + 1, -(beta - 1));
+                _board.UnmakeMove<TColor>(move);
+
+                if (value > bestValue)
+                {
+                    bestValue = value;
+
+                    if (value >= beta)
+                    {
+#if COLLECT_STATS
+                        _statistics.QCutNodes++;
+                        if (i == 0) { _statistics.FirstQCutNodes++; }
+#endif
+                        break;
+                    }
+                }
+            }
+
+            if (isInCheck && scoredMoves.Length == 0) { return -MateValue + ply; }
+
+            return bestValue;
+        }
     }
 }
